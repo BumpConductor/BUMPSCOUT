cfcce6ef9aecc006338c079bb825b6e5d905f7a6
bhargavkonkathi <bhargav.konkathi@gmail.com>
2017-12-20 14:29:58 +0530

    First Commit with Firebase auth manager.

---
+++ config.js
@@ -0,0 +1,13 @@
+window.Global = {
+    API_URL: 'https://us-central1-bumpscout-c2d7d.cloudfunctions.net/api/authenticated',
+    //API_URL: 'http://localhost:8080/authenticated',
+    firebase: {
+        apiKey: "AIzaSyCmchFDkooy8riNS9eHxDttuf8bUuwfRQc",
+        authDomain: "bumpscout-c2d7d.firebaseapp.com",
+        databaseURL: "https://bumpscout-c2d7d.firebaseio.com",
+        projectId: "bumpscout-c2d7d",
+        storageBucket: "bumpscout-c2d7d.appspot.com",
+        messagingSenderId: "640838690975",
+        gsURi:""
+    }
+};
